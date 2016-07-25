fe41836f1593b62e0a8f232e201ac06d23af695d
Daniel Trone <royhobbstn@yahoo.com>
2016-07-25 19:26:46 +0000

    to port 80

--- index.js
+++ index.js
@@ -2,16 +2,23 @@
 // docker run --name demogproxy -v /home/dola_gcp:/ssl/docker --link website:website -p 443:443 -d codemog/demog-proxy
 
 
-var sslobj={
-        port: 443,
-        key: 'home/dola_gcp/server.key',
-        cert: 'home/dola_gcp/server.crt'
-};
+// var sslobj={
+//         port: 443,
+//         key: 'home/dola_gcp/server.key',
+//         cert: 'home/dola_gcp/server.crt'
+// };
 
-var redbird = require('redbird')({
-  port: 443,
-  ssl: sslobj
-});
+// var redbird = require('redbird')({
+//   port: 443,
+//   ssl: sslobj
+// });
 
-redbird.register('demography.dola.colorado.gov/', 'http://website:4008', {ssl: true});
+// redbird.register('demography.dola.colorado.gov/', 'http://website:4008', {ssl: true});
 
+
+
+var proxy = require('redbird')({port: 80});
+
+
+// Route from hostnames as well as paths
+proxy.register("demography.dola.colorado.gov", "http://website:4008");
