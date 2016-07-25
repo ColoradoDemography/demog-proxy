7d63a38c280d04191186b061c9bdc1da98d3f404
Daniel Trone <royhobbstn@yahoo.com>
2016-07-25 20:30:14 +0000

    now ssl

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,6 @@ ADD . .
 # If you need npm, don't use a base tag
 RUN apk add --update bash && npm install
 
-EXPOSE 80
+EXPOSE 443
 
 CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -2,23 +2,20 @@
 // docker run --name demogproxy -v /home/dola_gcp:/ssl/docker --link website:website -p 443:443 -d codemog/demog-proxy
 
 
-// var sslobj={
-//         port: 443,
-//         key: 'home/dola_gcp/server.key',
-//         cert: 'home/dola_gcp/server.crt'
-// };
+var sslobj={
+        port: 443,
+        key: 'home/dola_gcp/server.key',
+        cert: 'home/dola_gcp/server.crt'
+};
 
-// var redbird = require('redbird')({
-//   port: 443,
-//   ssl: sslobj
-// });
+var redbird = require('redbird')({ port: 443, xfwd: false, ssl: sslobj });
 
-// redbird.register('demography.dola.colorado.gov/', 'http://website:4008', {ssl: true});
+redbird.register('23.236.54.199', 'http://website:4008', {ssl: true});
 
 
 
-var proxy = require('redbird')({port: 80, xfwd: false});
+// var proxy = require('redbird')({port: 80, xfwd: false});
 
 
-// Route from hostnames as well as paths
-proxy.register("http://23.236.54.199", "http://website:4008");
+// // Route from hostnames as well as paths
+// proxy.register("23.236.54.199", "http://website:4008", {ssl: true});
