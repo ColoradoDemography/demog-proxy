1dba2de3ce9989621103e98630f26d7a37c63775
Daniel Trone <royhobbstn@yahoo.com>
2016-07-26 14:13:04 +0000

    redirect http to https?

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ var sslobj={
 };
 
 var redbird = require('redbird')({ port: 443, xfwd: false, ssl: sslobj });
-
 redbird.register('demography.dola.colorado.gov', 'http://website:4008', {ssl: true});
 
 
-
+var http = require('redbird')({ port: 80 });
+http.register('demography.dola.colorado.gov', 'https://demography.dola.colorado.gov');
