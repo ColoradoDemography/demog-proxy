43ee88f20ece911163c62873e5c43eb27d684fbc
Daniel Trone <royhobbstn@yahoo.com>
2016-07-26 17:29:11 +0000

    here goes nothing

--- index.js
+++ index.js
@@ -1,33 +1,6 @@
 
-// docker run --name demogproxy -v /home/dola_gcp:/ssl/docker --link website:website -p 443:443 -p 80:80 -d codemog/demog-proxy
+// docker run --name demogproxy -v /etc/letsencrypt/live/demography.dola.colorado.gov:/ssl/docker --link website:website -p 443:443 -p 80:80 -d codemog/demog-proxy
 
-var LEX = require('letsencrypt-express').testing();
-
-// Change these two lines!
-var DOMAIN = 'demography.dola.colorado.gov';
-var EMAIL = 'royhobbstn@yahoo.com';
-
-console.log('homedir: ' + require('os').homedir());
-
-var lex = LEX.create({
-  configDir: require('os').homedir() + '/letsencrypt/etc'
-, approveRegistration: function (hostname, approve) { // leave `null` to disable automatic registration
-    if (hostname === DOMAIN) { // Or check a database or list of allowed domains
-      approve(null, {
-        domains: [DOMAIN]
-      , email: EMAIL
-      , agreeTos: true
-      });
-      continueprogram();
-    }
-  }
-});
-
-
-
-function continueprogram(){
-  
-  console.log('continue-ing');
   
 // http://heyrod.com/snippets/redirect-http-to-https-in-expressjs.html
 var http = require('http');
@@ -62,9 +35,9 @@ http.createServer(http_app).listen(HTTP_PORT).on('listening', function() {
 
 var sslobj={
         port: 443,
-        key: 'ssl/docker/server.key',
-        cert: 'ssl/docker/server.crt' //,  
-        //ca: 'ssl/docker/ChainBundle2.crt'
+        key: 'ssl/docker/privkey.pem',
+        cert: 'ssl/docker/fullchain.pem',  
+        ca: 'ssl/docker/chain.pem'
 };
 
 var redbird = require('redbird')({ port: 443, xfwd: false, ssl: sslobj });
@@ -73,4 +46,3 @@ redbird.register('demography.dola.colorado.gov', 'http://website:4008', {ssl: tr
 
 
   
-}
