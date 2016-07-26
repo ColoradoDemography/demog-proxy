0ecd202f8a122fe8aa26304f82c24bedce58be68
Daniel Trone <royhobbstn@yahoo.com>
2016-07-26 14:57:28 +0000

    add separate express sever to redirect http to https

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 
+
 // docker run --name demogproxy -v /home/dola_gcp:/ssl/docker --link website:website -p 443:443 -p 80:80 -d codemog/demog-proxy
+
+var http = require('http');
+var express = require('express');
+var HTTP_PORT  = 80;
+var HTTPS_PORT = 443;
+
+var http_app = express();
+http_app.set('port', HTTP_PORT);
+
+http_app.all('/*', function(req, res, next) {
+  if (/^http$/.test(req.protocol)) {
+    var host = req.headers.host.replace(/:[0-9]+$/g, ""); // strip the port # if any
+    if ((HTTPS_PORT != null) && HTTPS_PORT !== 443) {
+      return res.redirect("https://" + host + ":" + HTTPS_PORT + req.url, 301);
+    } else {
+      return res.redirect("https://" + host + req.url, 301);
+    }
+  } else {
+    return next();
+  }
+});
+
+
+http.createServer(http_app).listen(HTTP_PORT).on('listening', function() {
+  return console.log("HTTP to HTTPS redirect app launched.");
+});
+
+
 
 
 var sslobj={
@@ -13,5 +42,3 @@ var redbird = require('redbird')({ port: 443, xfwd: false, ssl: sslobj });
 redbird.register('demography.dola.colorado.gov', 'http://website:4008', {ssl: true});
 
 
-var http = require('redbird')({ port: 80 });
-http.register('demography.dola.colorado.gov', 'https://demography.dola.colorado.gov');
