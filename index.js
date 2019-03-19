de40869d1fdffaedc4beecad943305ac036c4368
TBleess <todd.bleess@gmail.com>
2019-03-19 11:28:05 -0600

    12 drummers drumming

--- index.js
+++ index.js
@@ -35,9 +35,9 @@ http.createServer(http_app).listen(HTTP_PORT).on('listening', function() {
 
 var sslobj={
         port: 443,
-        key: 'ssl/docker/privkey11.pem',
-        cert: 'ssl/docker/fullchain11.pem',  
-        ca: 'ssl/docker/chain11.pem'
+        key: 'ssl/docker/privkey12.pem',
+        cert: 'ssl/docker/fullchain12.pem',  
+        ca: 'ssl/docker/chain12.pem'
 };
 
 var redbird = require('redbird')({ port: 443, xfwd: false, ssl: sslobj });
