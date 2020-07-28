888c27411d8e9b546e60e9288b219752b6b934c3
TBleess <todd.bleess@gmail.com>
2020-07-28 12:45:24 -0600

    Dashboards to demogproxy

--- index.js
+++ index.js
@@ -42,5 +42,5 @@ var sslobj={
 
 var redbird = require('redbird')({ port: 443, xfwd: false, ssl: sslobj });
 redbird.register('demography.dola.colorado.gov', 'http://website:4008', {ssl: true});
-redbird.register('demography.dola.colorado.gov/apps', 'http://shiny-profiles:3838', {ssl: true});
+redbird.register('demography.dola.colorado.gov/apps', 'http://shiny-server:3838', {ssl: true});
 
