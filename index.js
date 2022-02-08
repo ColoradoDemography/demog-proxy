b7a4fcee281d8bdf2c16b5ae43c3e964f0d0a261
TBleess <todd.bleess@state.co.us>
2022-02-07 19:22:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -29,19 +29,6 @@ http.createServer(http_app).listen(HTTP_PORT).on('listening', function() {
   return console.log("HTTP to HTTPS redirect app launched.");
 });
 
-http_app.error(function(err, req, res, next){
-    if (err instanceof NotFound) {
-      console.log(req.url);
-        res.redirect('http://gis.dola.colorado.gov'+req.url);
-    } else {
-        next(err);
-    }
-});
-
-//http_app.use((req, res) => {
-//    res.status(404).redirect('http://gis.dola.colorado.gov'+req.url);
-//});
-
 // redbird
 
 var sslobj={
