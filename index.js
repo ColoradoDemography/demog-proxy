f1b3b4572180dd38bbfe5217ad9fef28eec75fc3
TBleess <todd.bleess@state.co.us>
2022-03-25 06:45:33 -0600

    Time for 25

--- index.js
+++ index.js
@@ -33,9 +33,9 @@ http.createServer(http_app).listen(HTTP_PORT).on('listening', function() {
 
 var sslobj={
         port: 443,
-        key: 'ssl/docker/privkey24.pem',
-        cert: 'ssl/docker/fullchain24.pem',  
-        ca: 'ssl/docker/chain24.pem',
+        key: 'ssl/docker/privkey25.pem',
+        cert: 'ssl/docker/fullchain25.pem',  
+        ca: 'ssl/docker/chain25.pem',
         secureOptions: constants.SSL_OP_NO_TLSv1 | constants.SSL_OP_NO_TLSv1_1
 };
 
