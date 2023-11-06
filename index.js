d8c09eab916d14dbef1745657d38a94b8e5e6bf3
TBleess <todd.bleess@state.co.us>
2023-11-06 08:41:26 -0700

    Update index.js SSL

--- index.js
+++ index.js
@@ -33,9 +33,9 @@ http.createServer(http_app).listen(HTTP_PORT).on('listening', function() {
 
 var sslobj={
         port: 443,
-        key: 'ssl/docker/privkey31.pem',
-        cert: 'ssl/docker/fullchain31.pem',  
-        ca: 'ssl/docker/chain30.pem',
+        key: 'ssl/docker/privkey32.pem',
+        cert: 'ssl/docker/fullchain32.pem',  
+        ca: 'ssl/docker/chain31.pem',
         secureOptions: constants.SSL_OP_NO_TLSv1 | constants.SSL_OP_NO_TLSv1_1
 };
 
