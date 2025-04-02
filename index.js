62435b4672c456de55b34db254028ce1a847687a
TBleess <todd.bleess@state.co.us>
2025-04-01 21:25:23 -0600

    April fools

--- index.js
+++ index.js
@@ -33,9 +33,9 @@ http.createServer(http_app).listen(HTTP_PORT).on('listening', function() {
 
 var sslobj={
         port: 443,
-        key: 'ssl/docker/privkey37.pem',
-        cert: 'ssl/docker/fullchain37.pem',  
-        ca: 'ssl/docker/chain36.pem',
+        key: 'ssl/docker/privkey38.pem',
+        cert: 'ssl/docker/fullchain38.pem',  
+        ca: 'ssl/docker/chain37.pem',
         secureOptions: constants.SSL_OP_NO_TLSv1 | constants.SSL_OP_NO_TLSv1_1
 };
 
