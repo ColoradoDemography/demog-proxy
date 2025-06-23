7966f507c6cbd71a13660b432c7e0b9d4f784678
TBleess <todd.bleess@state.co.us>
2025-06-23 13:02:51 -0600

    Update 39

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ http.createServer(http_app).listen(HTTP_PORT).on('listening', function() {
 
 var sslobj={
         port: 443,
-        key: 'ssl/docker/privkey38.pem',
-        cert: 'ssl/docker/fullchain38.pem',  
+        key: 'ssl/docker/privkey39.pem',
+        cert: 'ssl/docker/fullchain39.pem',  
         ca: 'ssl/docker/chain37.pem',
         secureOptions: constants.SSL_OP_NO_TLSv1 | constants.SSL_OP_NO_TLSv1_1
 };
